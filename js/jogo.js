7f05a3c3d36052a495e2ee0afa1ae501930bea78
GHOST-bot-wq <lj445657@gmail.com>
2026-08-28 14:19:29 -0300

    Add files via upload

--- js/jogo.js
+++ js/jogo.js
@@ -24,7 +24,7 @@ function renderJogo(container) {
     <div id="jogo-wrapper" style="position:relative;width:100%;height:100vh;overflow:hidden;background:#0a0a1a">
 
       <!-- iframe do jogo Three.js (src já inclui ?dif= e ?inf= e ?kco= para funcionar no mobile) -->
-      <iframe id="game-iframe" src="jogo/index.html?v=2.1.0&_t=${Date.now()}&dif=${encodeURIComponent(_difInicial)}&inf=${_infInicial}${_kcoInicial !== '' ? '&kco=' + encodeURIComponent(_kcoInicial) : ''}"
+      <iframe id="game-iframe" src="jogo/index.html?v=3.1.2&_t=${Date.now()}&dif=${encodeURIComponent(_difInicial)}&inf=${_infInicial}${_kcoInicial !== '' ? '&kco=' + encodeURIComponent(_kcoInicial) : ''}"
         style="width:100%;height:100%;border:none;display:block"
         allow="accelerometer; autoplay"
         title="Game">
